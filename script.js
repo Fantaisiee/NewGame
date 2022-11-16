f7cd1468ef4a118920bb43bf6af61e298085d5bf
Fantaisiee <113837292+Fantaisiee@users.noreply.github.com>
2022-11-16 14:13:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,14 +11,7 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT= canvas.height = 600;
 
 const playerImg = new Image();
-playerImg.onload = function(){
-    ctx .drawImage(
-    img,
-    0,
-    0,
-    canvas.width,
-    canvas.height
-};
+playerImg.onload = drawImage;
 playerImg.src = '/img/shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
